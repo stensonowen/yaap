28ef9ab34ba32a4e942725aef190e5fc4e9d05de
owen <owen@oms.sh>
2017-08-29 20:55:43 -0400

    Flesh out error type
    ehhh might just use an enum instead

--- src/err.rs
+++ src/err.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum ArgError {
@@ -8,5 +9,36 @@ pub enum ArgError {
     Repetition,         // argument set multiple times unexpectedly
 }
 
-pub type ArgResult<T> = Result<T, ArgError>;
+//pub type ArgResult<T> = Result<T, ArgError>;
 
+#[derive(Debug)]
+struct BadArg {
+    long: &'static str,
+    //short: Option<char>,
+    attempt: String, // maybe be ref ? // only makes sense to have attempts sometimes
+    problem: ArgError,
+}
+
+impl BadArg {
+    //pub fn from(
+}
+
+impl fmt::Display for BadArg {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use ArgError::*;
+        match self.problem {
+            UnexpectedValue => 
+                write!(f, "Flag `{}` cannot be set to a value (as in `{}`)",
+                       self.long, self.attempt),
+            MissingValue    => 
+                write!(f, "Option `{}` requires a value", self.long),
+            BadType         => 
+                write!(f, "Option `{}` expected type TODO (couldn't convert `{}`)",
+                       self.long, self.attempt),
+            MissingArg      => 
+                write!(f, "Missing required option `{}`", self.long),
+            Repetition      => 
+                write!(f, "Option `{}` cannot be set more than once", self.long),
+        }
+    }
+}
